7a75fd34cef154bdac3f892cede5cdfac9d5faa7
Jim Stewart <jim.stewart@thelangschool.net>
2025-10-22 08:31:17 -0400

    Student and Client class

---
+++ Client.py
@@ -0,0 +1,8 @@
+class Client:
+    def __init__(self, first, last, address, city, state, zipcode):
+        self.first = first
+        self.last = last
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
---
+++ Student.py
@@ -0,0 +1,5 @@
+class Student():
+    def __init__(self,first, last, grade):
+        self.first = first
+        self.last = last
+        self.grade = grade
